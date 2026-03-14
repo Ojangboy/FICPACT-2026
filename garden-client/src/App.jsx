69ece56c8ccdce1205b2ee75186d15886e258ab2
Ojangboy <fauzanfarhan126@gmail.com>
2026-03-15 00:49:46 +0700

    Add Register route to App component

--- garden-client/src/App.jsx
+++ garden-client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Onboarding from './components/Onboarding'
 import Login from './components/Login'
+import Register from './components/Register'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Onboarding />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         {/* Add more routes here later */}
       </Routes>
     </Router>
