29932dbb6aabcd23b890e52fc0ebde4b369741c5
Fauzanfedev <2310631250056@student.unsika.ac.id>
2026-03-14 22:57:06 +0700

    fixed projek

--- garden-client/tailwind.config.js
+++ garden-client/tailwind.config.js
@@ -5,12 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'garden-green': '#2D5A27',
-        'garden-brown': '#5C4033',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
