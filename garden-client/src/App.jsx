26411888cddc9c8b62ad65857bbe1e6a557b8d2d
Ojangboy <fauzanfarhan126@gmail.com>
2026-03-22 22:56:48 +0700

    Add Dashboard route to App component

--- garden-client/src/App.jsx
+++ garden-client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Onboarding from './components/Onboarding'
 import Login from './components/Login'
 import Register from './components/Register'
+import Dashboard from './components/Dashboard'
 import './App.css'
 
 function App() {
@@ -12,7 +13,10 @@ function App() {
         <Route path="/" element={<Onboarding />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
         {/* Add more routes here later */}
+
       </Routes>
     </Router>
   )
